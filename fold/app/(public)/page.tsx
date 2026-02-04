f49dec2889658d8bc3364d3997c2189d8f8b4a32
jeevant010 <jeevantmudgil10@gmail.com>
2026-02-04 11:57:11 +0530

    unwanted redundant

--- fold/app/(public)/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function PublicHomePage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-white">
-        Welcome to Spring Fiesta 2026
-      </h1>
-    </div>
-  );
-}
